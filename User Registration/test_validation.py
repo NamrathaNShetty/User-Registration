0aba4ff377704436a392ea7fcd07a211c0a997a3
NamrathaNShetty <namratanshetty@gmail.com>
2021-09-21 21:08:19 +0530

    [Namratha] Add: Validate all test cases

--- User Registration/test_validation.py
+++ User Registration/test_validation.py
@@ -3,7 +3,7 @@
 * @Date: 2021-09-21 01:11  
 * @Last Modified by: Namratha N Shetty
 * @Last Modified time: 2021-09-21 01:23
-* @Title: Solve test cases to validate first name.
+* @Title: Solve test cases for every user detail.
 '''
 
 from Validation import Validation 
@@ -32,3 +32,24 @@ class TestUserValidation(unittest.TestCase):
         self.assertFalse(Validation.validateFirstName("NAMRATHA"))
         self.assertFalse(Validation.validateFirstName("Namratha1"))
         self.assertFalse(Validation.validateFirstName("Namratha@"))
+
+    def test_givenValidLastName_shouldReturnTrue(self):
+        '''
+        Description:
+            The given valid last name should return true in this test case.
+        '''
+
+        self.assertTrue(Validation.validateLastName("Dinitha"))
+        self.assertTrue(Validation.validateLastName("Din"))
+
+    def test_givenInvalidLastName_shouldReturnFalse(self):
+        '''
+        Desription:
+            The given invalid last name should return false in this test case.
+        '''
+
+        self.assertFalse(Validation.validateLastName("Di"))
+        self.assertFalse(Validation.validateLastName("dinitha"))
+        self.assertFalse(Validation.validateLastName("DINITHA"))
+        self.assertFalse(Validation.validateLastName("Dinitha1"))
+        self.assertFalse(Validation.validateLastName("Dinitha@"))    
